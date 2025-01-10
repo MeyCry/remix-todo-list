34fd78ac083a10aa22a8acbf9cd410cc87f20e8b
meycry <timchukvadim@gmail.com>
2025-01-10 19:22:57 +0100

    add todo list with server handling
    (cherry picked from commit 8d30988f3a428f276bb5c70378da418002f119a0)

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -4,15 +4,35 @@
  * For more information, see https://remix.run/file-conventions/entry.client
  */
 
-import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
+import { RemixBrowser } from '@remix-run/react';
+import { startTransition, StrictMode } from 'react';
+import { hydrateRoot } from 'react-dom/client';
+
+window.requestIdleCallback =
+  window.requestIdleCallback ||
+  function (cb) {
+    const start = Date.now();
+    return setTimeout(function () {
+      cb({
+        didTimeout: false,
+        timeRemaining: function () {
+          return Math.max(0, 50 - (Date.now() - start));
+        },
+      });
+    }, 1);
+  };
+
+window.cancelIdleCallback =
+  window.cancelIdleCallback ||
+  function (id) {
+    clearTimeout(id);
+  };
 
 startTransition(() => {
   hydrateRoot(
     document,
     <StrictMode>
-      <RemixBrowser />
-    </StrictMode>
+      <RemixBrowser/>
+    </StrictMode>,
   );
 });
